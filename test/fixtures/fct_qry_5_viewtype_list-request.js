8be28088af32f375add44165a98e8fab1a157bb1
Carl Blakeley <cblakeley@openlinksw.com>
2020-03-03 18:03:27 +0000

    Initial support for setting a condition on a subject node.

---
+++ test/fixtures/fct_qry_5_viewtype_list-request.js
@@ -0,0 +1,10 @@
+export default `
+  <?xml version="1.0"?>
+  <query xmlns="http://openlinksw.com/services/facets/1.0">
+    <text property="http://www.openlinksw.com/ski_resorts/schema#description">skiing</text>
+    <property iri="http://www.openlinksw.com/ski_resorts/schema#advanced_slopes">
+      <view type="list" limit="50" offset="0">
+      </view>
+    </property>
+  </query>
+`
